a6ee7f838de7637913adee2e675b55b62ab52641
AxelLedung <axel.ledung@gmail.com>
2024-03-06 17:55:51 +0100

    Added barebone save test

--- src/test/java/org/example/VillageInputTest.java
+++ src/test/java/org/example/VillageInputTest.java
@@ -4,19 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 import java.io.ByteArrayInputStream;
-import java.io.PrintStream;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class VillageInputTest {
     DatabaseConnection dbMock;
@@ -25,26 +23,51 @@ class VillageInputTest {
         dbMock = mock(DatabaseConnection.class);
     }
     @Test
-    public void Save_Test() {
+    public void Save_CorrectSaving() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("These");
+        list.add("are");
+        list.add("placeholders");
+        list.add("to");
+        list.add("make");
+        list.add("sure");
+        list.add("it");
+        list.add("works");
+        when(dbMock.GetTownNames()).thenReturn(list);
         InputStream backup = System.in;
+        String input = "Save1";
 
-        
-
-        Village village = new Village();
-        boolean hej = dbMock.SaveVillage(village, "Save1");
-        assertEquals(true, hej );
-
-        Village village = new Village();
-
-        String myInput = "Save1";
-        ByteArrayInputStream in = new ByteArrayInputStream(myInput.getBytes());
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
+        Village village = new Village();
         VillageInput villageInput = new VillageInput(village, dbMock);
-        villageInput.scanner = new Scanner(System.in);
 
+        when(dbMock.SaveVillage(village, input)).thenReturn(true);
+
+        //Create backup before we redirect the outStream
+        PrintStream outBackup = System.out;
+        //Redirect the System.out.println to our variable: baos.
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        //Run villageInput.Save() which prints out the expected value into our baos variable.
         villageInput.Save();
-        //Assertions.assertEquals(1, 1);
+
+        //Remove the redirection to the original backup.
+        System.setOut(outBackup);
+
+        //Trim all the outputs to just our expected output.
+        //A lot of problem with line seperators which led to us using \r instead of \n
+        //More info about line seperators in: https://stackoverflow.com/questions/26926888/how-can-i-disable-diff-in-line-separators-in-intellij-idea
+        int trimLength = 30 + input.length();
+        String actual = baos.toString().substring(baos.toString().length() - trimLength, baos.toString().length() - 1);
+        String expected = "village " + input + " successfully saved.\r";
+
+        //Compare the trimmed output to the expected output.
+        assertEquals(expected.toLowerCase(), actual.toLowerCase());
+
+        System.setIn(backup);
     }
 
 }
